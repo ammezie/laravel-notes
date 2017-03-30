67a6f720aaac9101d599ee65b8eabfb325d78dc9
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-03-30 11:59:48 +0100

    install Pusher client

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -38,11 +38,13 @@ window.axios.defaults.headers.common = {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+import Echo from 'laravel-echo'
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '8d2eae921be61dbafd47',
+    cluster: 'eu',
+    encrypted: true
+});
